702e9467b759f423e2f2e82291fc94df60729717
Heitor Silva Rogerio <heitor.rogerio@unesp.br>
2026-05-30 18:33:38 -0300

    Update Bishop.java

--- src/pieces/pieces/Bishop.java
+++ src/pieces/pieces/Bishop.java
@@ -1,1 +1,51 @@
+package chess.pieces;
 
+import boardgame.*;
+import chess.*;
+
+public class Bishop extends Piece {
+
+    public Bishop(Board board, Color color) {
+        super(board, color, PieceType.BISHOP);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+
+        boolean[][] mat =
+                new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        int[][] directions = {
+                {-1,-1},
+                {-1,1},
+                {1,-1},
+                {1,1}
+        };
+
+        for (int[] d : directions) {
+
+            Position p =
+                    new Position(
+                            position.getRow()+d[0],
+                            position.getColumn()+d[1]);
+
+            while (getBoard().positionExists(p)
+                    && !getBoard().hasPiece(p)) {
+
+                mat[p.getRow()][p.getColumn()] = true;
+
+                p = new Position(
+                        p.getRow()+d[0],
+                        p.getColumn()+d[1]);
+            }
+
+            if (getBoard().positionExists(p)
+                    && isThereOpponentPiece(p)) {
+
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
+        return mat;
+    }
+}
